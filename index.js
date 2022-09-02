8b1a0d281cdeeafaa027db18f2517f3355f14460
Renan <renanandreolla@outlook.com>
2022-09-01 22:13:16 -0300

    Basic structure on node

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// SERVER CONFIGURATION
+var port = process.env.PORT || 3000;
+
+let env = nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('engine', env);
+require('useful-nunjucks-filters')(env);
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+    extended: true
+}));
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`node app listening on port ${port}`)
-})
+    console.log('info', 'LISTEN ON PORT ' + port);
+});
+
+app.get('/teste', (req, res) => {
+  res.render('teste.html')
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html')
+});
