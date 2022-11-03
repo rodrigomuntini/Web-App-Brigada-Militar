be6e9aac23caa8ed2f5dbcd21f9a221e0648c20b
MarlonAngonese <marlonangonese.dev@gmail.com>
2022-11-03 17:22:49 -0300

    Include visits page

--- index.js
+++ index.js
@@ -100,15 +100,36 @@ app.get("/home", (req, res) => {
   res.render("home.html");
 });
 
-app.get("/users", auth, (req, res) => {
+// Users
+app.get("/users", (req, res) => {
+
+  // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjcxNjgyMzEsImV4cCI6MTY2NzE4OTgzMSwibmJmIjoxNjY3MTY4MjMxLCJqdGkiOiI5MkVMMjZSOW5MSE1UR2xLIiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.omwW3-31UZ40SvTF3-1rgmUc1JqKHIOOsmS78Z8vZEM";
+
+  let success_message = "";
+
+  if (req.session.success_message) {
+    success_message = req.session.success_message;
+    delete req.session.success_message;
+  }
+
+  let page = req.query.page || 1;
+  let search = req.query.search ? "&search=" + req.query.search : "";
+
   $.ajax({
     type: "GET",
-    url: "https://novo-rumo-api.herokuapp.com/api/users",
+    url: "http://localhost:8000/api/users?page=" + page + search,
     headers: {
       'Authorization': 'bearer ' + req.session.token
     },
     success: function (data) {
-      return res.render("users/list.html", { users: data.users });
+      return res.render("users/list.html",{
+        users: data.users,
+        message: success_message,
+        page: data.page,
+        last_page: data.last_page,
+        total: data.total,
+        search: data.search
+      });
     },
     error: function (error) {
       return res.redirect("/login?error=Unauthorized");
@@ -121,5 +142,118 @@ app.get("/users/add", (req, res) => {
 });
 
 app.post("/users/add", (req, res) => {
-  // res.render('users/add.html');
+
+  // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjY5NjA1ODMsImV4cCI6MTY2Njk4MjE4MywibmJmIjoxNjY2OTYwNTgzLCJqdGkiOiJlSVpidUowOWwzYUVwWE5UIiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.T9NWr14eqtF1tdHcLBcbA0ZXQij-lwdjY4N008wXSK0";
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8000/api/users/add",
+    headers: {
+      'Authorization': 'bearer ' + req.session.token
+    },
+    data: req.body,
+    success: function (data) {
+      req.session.success_message = "Servidor criado com sucesso!"
+      return res.redirect('/users');
+    },
+    error: function (error) {
+      console.log(error);
+      return res.render('users/add.html', { 'request_body': req.body, 'error': error.responseJSON.error })
+    },
+  });
+});
+
+app.get("/users/edit/:id", (req, res) => {
+  // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjcxNjgyMzEsImV4cCI6MTY2NzE4OTgzMSwibmJmIjoxNjY3MTY4MjMxLCJqdGkiOiI5MkVMMjZSOW5MSE1UR2xLIiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.omwW3-31UZ40SvTF3-1rgmUc1JqKHIOOsmS78Z8vZEM";
+
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8000/api/users/show/" + req.params.id,
+    headers: {
+      'Authorization': 'bearer ' + req.session.token
+    },
+    success: function (data) {
+      res.render("users/edit.html", { 'request_body': data.user });
+    },
+  });
+});
+
+app.post("/users/edit/:id", (req, res) => {
+  // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjcxNjgyMzEsImV4cCI6MTY2NzE4OTgzMSwibmJmIjoxNjY3MTY4MjMxLCJqdGkiOiI5MkVMMjZSOW5MSE1UR2xLIiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.omwW3-31UZ40SvTF3-1rgmUc1JqKHIOOsmS78Z8vZEM";
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8000/api/users/edit/" + req.params.id,
+    headers: {
+      'Authorization': 'bearer ' + req.session.token
+    },
+    data: req.body,
+    success: function (data) {
+      console.log(data);
+      req.session.success_message = "Servidor alterado com sucesso!"
+      return res.redirect('/users');
+    },
+    error: function (error) {
+      console.log(error);
+      req.body._id = req.params.id;
+      return res.render('users/edit.html', { 'request_body': req.body, 'error': error.responseJSON.error })
+    },
+  });
+});
+
+app.post("/users/delete/:id", (req, res) => {
+  // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjcxNjgyMzEsImV4cCI6MTY2NzE4OTgzMSwibmJmIjoxNjY3MTY4MjMxLCJqdGkiOiI5MkVMMjZSOW5MSE1UR2xLIiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.omwW3-31UZ40SvTF3-1rgmUc1JqKHIOOsmS78Z8vZEM";
+
+  $.ajax({
+    type: "DELETE",
+    url: "http://localhost:8000/api/users/delete/" + req.params.id,
+    headers: {
+      'Authorization': 'bearer ' + req.session.token
+    },
+    success: function (data) {
+      console.log(data);
+      return res.send(true);
+    },
+    error: function (error) {
+      console.log(error);
+      return res.send(false);
+    },
+  });
+});
+
+// Visits
+app.get("/visits", (req, res) => {
+
+  req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2Njc0OTc2NzYsImV4cCI6MTY2NzUxOTI3NiwibmJmIjoxNjY3NDk3Njc2LCJqdGkiOiJWUFhIZHBSaVZpR2RkYUE0Iiwic3ViIjoiNjMzODgwMGM2NmE4ZDhkYjA4MDQyODEyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.kylvyuj5QAIeoLafYMxL1bi5UBWgUaRUHA108K0z6rI";
+
+  let success_message = "";
+
+  if (req.session.success_message) {
+    success_message = req.session.success_message;
+    delete req.session.success_message;
+  }
+
+  let page = req.query.page || 1;
+  let search = req.query.search ? "&search=" + req.query.search : "";
+
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8000/api/visits?page=" + page + search,
+    headers: {
+      'Authorization': 'bearer ' + req.session.token
+    },
+    success: function (data) {
+      return res.render("visits/list.html",{
+        visits: data.visits,
+        message: success_message,
+        page: data.page,
+        last_page: data.last_page,
+        total: data.total,
+        search: data.search
+      });
+    },
+    error: function (error) {
+      return res.redirect("/login?error=Unauthorized");
+    },
+  });
 });
