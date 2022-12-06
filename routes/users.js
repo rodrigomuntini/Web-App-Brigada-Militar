e6db867b3c8f1a507a2edfefbd3ef787da5aaad1
MarlonAngonese <marlonangonese.dev@gmail.com>
2022-12-05 23:54:54 -0300

    Set filter by time in users view

--- routes/users.js
+++ routes/users.js
@@ -139,12 +139,12 @@ router.post("/edit/:id", (req, res) => {
         },
         data: req.body,
         success: function (data) {
-            console.log(data);
+            // console.log(data);
             req.session.success_message = "Servidor alterado com sucesso!"
             return res.redirect('/users');
         },
         error: function (error) {
-            console.log(error);
+            // console.log(error);
             req.body._id = req.params.id;
             return res.render('users/edit.html', { 'request_body': req.body, 'error': error.responseJSON.error })
         },
@@ -161,11 +161,11 @@ router.post("/delete/:id", (req, res) => {
             'Authorization': 'bearer ' + req.session.token
         },
         success: function (data) {
-            console.log(data);
+            // console.log(data);
             return res.send(true);
         },
         error: function (error) {
-            console.log(error);
+            // console.log(error);
             return res.send(false);
         },
     });
@@ -174,19 +174,22 @@ router.post("/delete/:id", (req, res) => {
 router.get("/view/:id", (req, res) => {
     // req.session.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbm92by1ydW1vLWFwaS5oZXJva3VhcHAuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjY3OTU3MDU0LCJleHAiOjE2Njc5Nzg2NTQsIm5iZiI6MTY2Nzk1NzA1NCwianRpIjoicFpjczR5b29CMmNDbFh0SSIsInN1YiI6IjYzMzg4MDBjNjZhOGQ4ZGIwODA0MjgxMiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.nLBDtJPwwVnu3aN39wZZthXsafvjXvDRuzt1HArl7Yc";
 
+    let from = req.query.from ? "&from=" + req.query.from : "";
+    let to = req.query.to ? "&to=" + req.query.to : "";
+
     $.ajax({
         type: "GET",
-        url: "http://0.0.0.0:8002/api/users/show/" + req.params.id,
+        url: "http://0.0.0.0:8002/api/users/show/" + req.params.id + "?" + from + to,
         headers: {
             'Authorization': 'bearer ' + req.session.token
         },
         success: function (data) {
-            console.log(data)
-            console.log(data.user.visits)
+            // console.log(data)
+            // console.log(data.user.visits)
             res.render("users/view.html", { 'user': data.user });
         },
         error: function (error) {
-            console.log(error);
+            // console.log(error);
             return res.redirect("/login?error=Unauthorized");
         },
     });
